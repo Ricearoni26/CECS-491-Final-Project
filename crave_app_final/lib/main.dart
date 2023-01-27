a7f22d14bf78b052289f950114063249d340d83a
Michael Ching <michaeljune101@yahoo.com>
2023-01-27 13:05:20 -0800

    Added firebase configs

--- crave_app_final/lib/main.dart
+++ crave_app_final/lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:crave_app_final/screens/login_screen.dart';
 import 'package:flutter/material.dart';
-//import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 //import 'package:firebase_storage/firebase_storage.dart';
-//import 'firebase_options.dart';
+import 'firebase_options.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
+
 
 // void main() async {
 //   WidgetsFlutterBinding.ensureInitialized();
