407b6e1ba19bc13eb1a0d2ddeeed758b061188dd
Ricearoni26 <joey.rice01@student.csulb.edu>
2023-02-16 16:38:42 -0800

    Adding Googleinfo.plist

--- crave_app_final/lib/screens/home_screen.dart
+++ crave_app_final/lib/screens/home_screen.dart
@@ -1,6 +1,9 @@
 import 'package:crave_app_final/screens/login_screen.dart';
 import 'package:firebase_database/firebase_database.dart';
+<<<<<<< Updated upstream
 import 'signin_screen.dart';
+=======
+>>>>>>> Stashed changes
 import 'package:crave_app_final/screens/signin_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
