1b7b64c8330995cdfc9571014f57ce50f8afd922
Michael Ching <michaeljune101@yahoo.com>
2023-04-05 21:04:25 -0700

    try to read from DB

--- crave_app_final/lib/screens/history_screen.dart
+++ crave_app_final/lib/screens/history_screen.dart
@@ -1,10 +1,55 @@
 import 'package:flutter/material.dart';
 
+import 'dart:async';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+
+
 class HistoryScreen extends StatelessWidget {
-  const HistoryScreen({Key? key}) : super(key: key);
+  final DatabaseReference ref = FirebaseDatabase.instance.ref();
+
+
+  Future<String> displayUserDetails() async {
+    final String uid = FirebaseAuth.instance.currentUser!.uid;
+    final DatabaseReference refUser = FirebaseDatabase.instance.ref('users/$uid');
+
+    final Completer<String> completer = Completer<String>();
+
+    refUser.onValue.listen((event) {
+      final data = event.snapshot.value as Map<dynamic, dynamic>;
+      final fullName = '${data['firstName']} ${data['lastName']} ${data['preferences']['Additional services']} ';
+      print('Full name: $fullName');
+      completer.complete(fullName);
+    });
+
+    final String fullName = await completer.future;
+    print('Returning full name: $fullName');
+    return fullName;
+  }
+
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Food History'),
+      ),
+      body: Center(
+        child: FutureBuilder<String>(
+          future: displayUserDetails(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return CircularProgressIndicator();
+            }
+            if (snapshot.hasError) {
+              return Text('Error: ${snapshot.error}');
+            }
+            return Text(snapshot.data ?? '');
+          },
+        ),
+      ),
+    );
   }
 }
