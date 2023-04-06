e6fe6a1bb068039c70e9fc190b9ad4047d197683
Ricearoni26 <joey.rice01@student.csulb.edu>
2023-04-06 14:11:36 -0700

    Small changes

--- crave_app_final/lib/screens/preferences_screen.dart
+++ crave_app_final/lib/screens/preferences_screen.dart
@@ -3,6 +3,7 @@ import 'package:crave_app_final/screens/home_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:multi_select_flutter/multi_select_flutter.dart';
 
 class PreferencesScreen extends StatefulWidget {
   @override
